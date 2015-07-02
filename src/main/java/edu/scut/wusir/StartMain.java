a7e2049f9589839fbddd09d74c77ca1a83ea3500
wuyinxian124 <wuyinxian124@gmail.com>
2015-07-02 17:01:59 +0800

    去编译警告

--- src/main/java/edu/scut/wusir/StartMain.java
+++ src/main/java/edu/scut/wusir/StartMain.java
@@ -1,9 +1,7 @@
 package edu.scut.wusir;
 
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
